2dddaa6c8dc4ee823f9533cf07ab951bf02ce480
bobo <2284650586@qq.com>
2022-06-02 14:18:23 +0800

    1. 项目初始化

---
+++ src/test/java/com/lxt/EladminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
